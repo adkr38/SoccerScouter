a2989e5ef162d301b64887aa37e42e7828698ee0
adriankr38 <adrian58ago@gmail.com>
2023-08-26 14:05:24 +0100

    add(js) `Hero` pan design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import {ThemeContext} from "../context/ThemeContext"
+import {TextContext} from "../context/TextContext"
+import Pan from "./Pan"
+import * as ai from "react-icons/ai"
+import * as gi from "react-icons/gi"
+import * as lu from "react-icons/lu"
+import * as tb from "react-icons/tb"
 
 const Hero = () =>{
 
 
     const {theme,toggleTheme} = React.useContext(ThemeContext)
-    const h1Styling =
-        theme ==="light"
-    ?"text-2xl text-neutral-800"
-    :"text-2xl text-neutral-200"
+    const {textMode,toggleTextMode} = React.useContext(TextContext)
+
 
     const bgStyling =
-
         theme ==="light"
-    ?"animate-fade-out-semi w-[300%] h-[300%]  bg-[url('../autumn.svg')] opacity-[2%] rotate-45 scale-[1.5] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-center"
-    :"animate-fade-out-semi w-[300%] h-[300%]  bg-[url('../autumn.svg')] opacity-[2%] rotate-45 scale-[1.5] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-center invert"
+    ?"animate-fade-out-semi w-[300%] h-[300%]  bg-[url('../autumn.svg')] opacity-[1%] rotate-45 scale-[1.5] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-center"
+    :"animate-fade-out-semi w-[300%] h-[300%]  bg-[url('../autumn.svg')] opacity-[1%] rotate-45 scale-[1.5] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-center invert"
+
+
+    const pStyling =
+        theme ==="light"
+    ?"text-lg text-neutral-700 font-cmu"
+    :"text-lg text-neutral-200 font-cmu"
+
+    const iconStyling=
+        theme ==="light"
+    ?"text-5xl sm:text-8xl text-neutral-600"
+    :"text-5xl sm:text-8xl text-neutral-400"
+
+    const badgeContainer=
+        theme ==="light"
+    ?"shadow-sm shadow-neutral-200 w-full h-full  border border-neutral-200 flex items-center justify-center"
+    :"shadow-sm shadow-neutral-800 w-full h-full  border border-neutral-600 flex items-center justify-center"
+
 
 
     return(
@@ -23,13 +43,68 @@ const Hero = () =>{
         <div className={bgStyling}>
 
 
-</div>
-
-        <div className="z-10 text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className={h1Styling}>
-            Welcome to SoccerScout!
-        </h1>
         </div>
+
+
+        <Pan boxStyling="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 h-3/4" innerStyling=" grid grid-cols-2 grid-rows-3 sm:grid-cols-3 sm:grid-rows-2 w-full h-full items-center align-middle place-items-center">
+        <div className={badgeContainer}>
+        {
+            textMode
+            ?<p className={pStyling}> Teams</p>
+            :<ai.AiOutlineTeam className={iconStyling}/>
+        }
+        </div>
+
+        <div className={badgeContainer}>
+        {
+            textMode
+            ?<p className={pStyling}>Players</p>
+            :<ai.AiOutlineUser className={iconStyling}/>
+        }
+        </div>
+        <div className={badgeContainer}>
+
+        {
+            textMode
+            ?<p className={pStyling}>Competitions</p>
+            :<ai.AiOutlineTrophy className={iconStyling}/>
+        }
+
+        </div>
+
+        <div className={badgeContainer}>
+
+        {
+            textMode
+            ?<p className={pStyling}>Attacking</p>
+            :<gi.GiSoccerKick className={iconStyling}/>
+        }
+        </div>
+
+        <div className={badgeContainer}>
+
+        {
+            textMode
+            ?<p className={pStyling}>Defending</p>
+            :<lu.LuShield className={iconStyling}/>
+        }
+        </div>
+
+        <div className={badgeContainer}>
+
+        {
+            textMode
+            ?<p className={pStyling}>Ball Controll</p>
+            :<tb.TbPlayFootball className={iconStyling}/>
+        }
+        </div>
+        </Pan>
+
+
+
+
+
+
 
 </div>
 
